6992bac23c6a73de963c1ba26c01dd64303fc48f
HaticeBilici61 <bilicihatice1987@gmail.com>
2023-02-18 11:16:19 +0100

    b2

--- src/test/java/techproed/utilities/ListenersRetry.java
+++ src/test/java/techproed/utilities/ListenersRetry.java
@@ -4,9 +4,12 @@ import org.testng.IRetryAnalyzer;
 import org.testng.ITestResult;
 public class ListenersRetry implements IRetryAnalyzer {
     private int retryCount = 0;
-    private static final int maxRetryCount = 2;
+    private static final int maxRetryCount = 1;
+    private ITestResult result;
+
     @Override
     public boolean retry(ITestResult result) {
+        this.result = result;
         if (retryCount < maxRetryCount) {
             retryCount++;
             return true;
